40e4feb2bfb9609c7c362cc5cc064f9d613afe0b
Jiri Vyhlas <jirivyhlas@seznam.cz>
2020-07-22 19:24:05 +0200

    commit on 'master'

--- index.php
+++ index.php
@@ -54,6 +54,7 @@ $stranka = "osetreni";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $poleStranek[$stranka]["titulek"]?></title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700&display=swap" rel="stylesheet">
@@ -100,6 +101,8 @@ $stranka = "osetreni";
 </div>
 
 </header>
+
+
 
 <section>
  
@@ -151,6 +154,8 @@ $stranka = "osetreni";
 
     </div>
 </footer>
+<script src="https://code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/grid-gallery.min.js"></script>
 
 </body>
